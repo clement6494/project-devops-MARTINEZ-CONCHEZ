b1c589336a51dbfc6239d9c5ce6fea83791f6c80
clement <clement.conchezboueytou@edu.ece.fr>
2022-12-14 02:43:32 +0100

    feat: get method

--- userapi/src/controllers/user.js
+++ userapi/src/controllers/user.js
@@ -10,12 +10,24 @@ module.exports = {
       firstname: user.firstname,
       lastname: user.lastname,
     }
-    // Save to DB
+    
     // TODO check if user already exists
-    db.hmset(user.username, userObj, (err, res) => {
+    db.hgetall(user.username, function(err , res) {
       if (err) return callback(err, null)
-      callback(null, res) // Return callback
+      if(!res) {
+        // i.e. user doesn t exist
+        // Save to DB
+        db.hmset(user.username, userObj, (err, res) => {
+          if (err) return callback(err, null)
+          callback(null, res) // Return callback
+        })
+      } else {
+        callback( new Error("user already exists"), null )
+      }
+
     })
+    
+    
   },
   // get: (username, callback) => {
   //   // TODO create this method
@@ -25,6 +37,13 @@ module.exports = {
     if(!user.username)
       return callback(new Error("Please enter a user name"), null)
     // get User info
-    return callback()
+    db.hgetall(username, function(err , res) {
+      if (err) return callback(err, null)
+      if (res)
+        callback(null,res)
+      else 
+        callback(new Error("user doesn t exist"), null ) 
+
+    })
   }
 }
