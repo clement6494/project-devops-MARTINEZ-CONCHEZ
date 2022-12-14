f76c6a531bf6304931085836ecfb4a0de0d1fa6a
clement <clement.conchezboueytou@edu.ece.fr>
2022-12-14 02:31:50 +0100

    feat: get method API

--- userapi/src/routes/user.js
+++ userapi/src/routes/user.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const user = require('../controllers/user')
 const userController = require('../controllers/user')
 
 const userRouter = express.Router()
@@ -25,5 +26,25 @@ userRouter
   //   // TODO Create get method API
   //   const username = req.params.username
   // })
+
+  .get('/:username', (req, resp, next)  => {
+    const username = req.params.username
+    userController.get(username, (err,res) => {
+      let respObj
+      if(err) {
+        respObj = {
+          status: "error",
+          msg: err.message
+        }
+        return resp.status(400).json(respObj)
+      }
+      respObj = {
+        status: "success",
+        msg: res
+      }
+      resp.status(200).json(resObj)
+    })
+
+  })
   
 module.exports = userRouter
