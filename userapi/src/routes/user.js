b9ad0787f992b78a4643ec4e4261a1681eda2f4b
clement <clement.conchezboueytou@edu.ece.fr>
2022-12-17 01:13:13 +0100

    feat: put and delete API method

--- userapi/src/routes/user.js
+++ userapi/src/routes/user.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const user = require('../controllers/user')
 
 const userController = require('../controllers/user')
 
@@ -44,6 +45,47 @@ userRouter
         msg: res
       }
       resp.status(200).json(respObj)
+    })
+
+  })
+
+  .put('/', (req, resp) => {
+    userController.put(req.body, (err, res) => {
+      let respObj
+      if(err) {
+        respObj = {
+          status: "error",
+          msg: err.message
+        }
+        return resp.status(400).json(respObj)
+      }
+      respObj = {
+        status: "success",
+        msg: res
+      }
+      resp.status(201).json(respObj)
+    })
+  })
+
+  .delete('/:username',(req,resp) => {
+    const usertodelete = req.params.username   // will only delete username and so the route of access to account need to dele namm firstname
+
+    userController.delete(usertodelete, (err,res) => {
+      let respObj
+      if(err) {
+        respObj = {
+          status: "error",
+          msg: err.message
+        }
+        return resp.status(400).json(respObj)
+      }
+
+      respObj = {
+        status: "success",
+        msg: res
+      }
+      resp.status(200).json(respObj)
+
     })
 
   })
