53bdab23c0d5429e407267fa6df9a20f7e49f9f2
clement <clement.conchezboueytou@edu.ece.fr>
2022-12-27 15:57:53 +0100

    feat: docker node

--- userapi/src/index.js
+++ userapi/src/index.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({
 }))
 app.use(bodyParser.json())
 
-app.get('/', (req, res) => res.send('Hello sir!'))
+app.get('/', (req, res) => res.send('Hello World!'))
 
 app.use('/user', userRouter)
 
